639b24472caf9fe7a17bb41c9b3f3bd8c63f1d2d
kmbabu <40886017+kmbabu@users.noreply.github.com>
2018-07-06 23:38:51 +0530

    Add files via upload

---
+++ sss.py
@@ -0,0 +1,13 @@
+ch=raw_input("enter the charecter")
+if(ch=="a"):
+    print("ch is a vowel")
+elif(ch=="e"):
+   print("ch is a vowel")
+elif(ch=="i"):
+    print("ch is a vowel")
+elif(ch=="o"):
+    print("ch is a vowel")
+elif(ch=="u"):
+     print("ch is an vowel")
+else:
+    print("ch is a consonent")
